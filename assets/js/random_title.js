b9f7ffc0af4309a060993296b85de3682f8e616c
arttostog <38782266+arttostog@users.noreply.github.com>
2023-09-17 17:59:03 +0500

    fix: random_title.js update

--- assets/js/random_title.js
+++ assets/js/random_title.js
@@ -1,4 +1,4 @@
-var Titles = ["Arttostog's personal website", "Hi there 👋", "Today is wednesday?", "Honk!", "🐝", "☔", "F!", "Yeap", "Ping!", "Pong!", "📢"];
+var Titles = ["Arttostog's personal website", "Hi there 👋", "Today is wednesday?", "Honk!", "🐝", "☔", "F!", "Yea", "Ping!", "Pong!", "📢"];
 
 function RandomTitle() {
     return Titles[Math.floor(Math.random() * Titles.length)];
