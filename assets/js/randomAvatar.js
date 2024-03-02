132448fd7e906bdbcb1907feebaa6af1ce094ce5
arttostog <arttostog@gmail.com>
2024-03-02 18:09:47 +0500

    Website update

--- assets/js/randomAvatar.js
+++ assets/js/randomAvatar.js
@@ -1,4 +1,4 @@
-var avatars = ["./assets/images/avatar.jpg", "./assets/images/avatar2.png"]
+const avatars = ["./assets/images/avatar.jpg", "./assets/images/avatar2.png"]
 
 function getRandomAvatar() {
     if (Math.floor(Math.random() * 100) == 100) {
@@ -7,6 +7,6 @@ function getRandomAvatar() {
     return avatars[0];
 }
 
-var avatar = getRandomAvatar();
+let avatar = getRandomAvatar();
 document.getElementById("favicon").setAttribute("href", avatar);
 document.getElementById("icon").setAttribute("src", avatar);
--- assets/js/randomTitle.js
+++ assets/js/randomTitle.js
@@ -1,7 +1,3 @@
-var Titles = ["Arttostog's personal website", "Hi there 👋", "Today is wednesday?", "Honk!", "🐝", "☔", "F!", "Yea", "Ping!", "Pong!", "📢"];
+const Titles = ["Arttostog's personal website", "Hi there 👋", "Today is wednesday?", "Honk!", "🐝", "☔", "F!", "Yea", "Ping!", "Pong!", "📢"];
 
-function getRandomTitle() {
-    return Titles[Math.floor(Math.random() * Titles.length)];
-}
-
-document.title = getRandomTitle();
+document.title = Titles[Math.floor(Math.random() * Titles.length)];
