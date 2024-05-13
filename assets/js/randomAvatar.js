a12793be87c5602a70fe4703b6fe9f8f2b98d765
arttostog <arttostog@gmail.com>
2024-05-13 17:41:29 +0500

    Website update

--- assets/js/randomAvatar.js
+++ assets/js/randomAvatar.js
@@ -1,9 +1,8 @@
 const avatars = ["./assets/images/avatar.jpg", "./assets/images/avatar2.png"]
 
 function getRandomAvatar() {
-    if (Math.floor(Math.random() * 100) == 100) {
+    if (Math.floor(Math.random() * 100) == 100)
         return avatars[1];
-    }
     return avatars[0];
 }
 
